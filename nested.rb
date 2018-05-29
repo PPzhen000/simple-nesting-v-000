d113935b86824d5783215265f266fced246225a7
Ariel Zhen <zhenpingping@live.com>
2018-05-29 07:55:18 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -84,6 +84,8 @@ def adding_matz
      }
 		 programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby",  :languages => ["LISP", "C"]}
 
+		 programmer_hash
+
 end
 
 def changing_alan
@@ -104,6 +106,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      programmer_hash[:alan_kay][:known_for] = "GUI"
+
+		 programmer_hash
 
 
 end
